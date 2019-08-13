bd7fe5df1acf4b62cc3a83718fa73c567c3c777b
John Mateer <jj.mateer@live.com>
2019-08-13 04:29:55 -0700

    Used a switch with user input 1 to call on different functions. So far, concert-this does not get a response but spotify this song successfully pulls data.

--- liri.js
+++ liri.js
@@ -1,31 +1,44 @@
 require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
-var spotify = keys.spotify;
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 var input1 = process.argv[2];
 var input2 = process.argv[3];
-var queryURL = "https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=codingbootcamp"
+var input3 = process.argv[4];
+var queryURL1 = "https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=codingbootcamp"
+// var queryURL2 = "https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx"
 switch (input1) {
-    case "concert-this":
+    //concert-this
+    case "ct":
         concert();
         break;
-
-    case "spotify-this-song":
+    //spotify-this-song
+    case "sts":
+        spotifySong()
         break;
-
-    case "movie-this":
+    //movie-this
+    case "mt":
         break;
-
+    //do-what-it-says
     case "do-what-it-says":
         break;
 }
 function concert() {
-    axios.get(queryURL).then(
+    axios.get(queryURL1).then(
         function (response) {
             console.log(response)
             // console.log(JSON.stringify(response, null, 2));
         })
 }
 function spotifySong() {
+    spotify.search({ type: input2, query: input3, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data.name);
+        console.log(JSON.stringify(data, null, 2));
+    });
     
 }
