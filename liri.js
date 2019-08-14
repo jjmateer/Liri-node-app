691e90e06797f61f7663cf175541be8909533bd5
John Mateer <jj.mateer@live.com>
2019-08-13 21:45:12 -0700

    Used for loop in concert-this to console log all of the venues, locations and event dates throughout the length of the response data. Replaced input1/input2 with the multWord variable which joins all words after argv[3] and joins them together in a string that divides the words with plus signs

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+var moment = require('moment');
+moment().format();
 var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -6,21 +8,17 @@ var request = require("request");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var nodeArgs = process.argv;
-var input1 = process.argv[2];
-var input2 = process.argv[3];
-var queryURL1 = "https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=codingbootcamp"
-// var queryURL2 = "https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx"
-var queryUrl = "http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy";
-// var SongName = "";
-// for (var i = 3; i < nodeArgs.length; i++) {
-//     if (i > 3 && i < nodeArgs.length) {
-//         SongName = SongName + "+" + nodeArgs[i];
-//     }
-//     else {
-//         SongName += nodeArgs[i];
-//     }
-// }
-switch (input1) {
+var input = process.argv[2];
+var multWord = "";
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        multWord = multWord + "+" + nodeArgs[i];
+    }
+    else {
+        multWord += nodeArgs[i];
+    }
+}
+switch (input) {
     //concert-this
     case "ct":
         concert();
@@ -39,19 +37,29 @@ switch (input1) {
         break;
 }
 function concert() {
-    axios.get(queryURL1).then(
-        function (body) {
-            console.log(body)
+    axios.get("https://rest.bandsintown.com/artists/" + multWord + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            // if (err) {
+            //     return console.log('Error occurred: ' + err);
+            // }
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Searched: " + multWord)
+                console.log("Venue Name: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+                console.log("Date of Event: " + moment(response.data[i].datetime).format("L"));
+                console.log('=======================================')
+            }
 
         })
 }
 function spotifySong() {
-    spotify.search({ type: 'track', query: input2, limit: 1 }, function (err, data) {
+    spotify.search({ type: 'track', query: multWord, limit: 1 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
         for (var i = 0; i < data.tracks.items.length; i++) {
             var songData = data.tracks.items[i];
+            console.log('Searched: ' + multWord)
             console.log("Artist: " + songData.artists[0].name);
             console.log("Song Title: " + songData.name);
             console.log("Preview Track: " + songData.preview_url);
@@ -61,7 +69,7 @@ function spotifySong() {
 }
 
 function movie() {
-    request(queryUrl, function (err, response, body) {
+    request("http://www.omdbapi.com/?t=" + multWord + "&y=&plot=short&apikey=trilogy", function (err, response, body) {
         console.log("Title: " + JSON.parse(body).Title);
         console.log("Release Year: " + JSON.parse(body).Year);
         console.log("Rating: " + JSON.parse(body).imdbRating);
@@ -70,26 +78,31 @@ function movie() {
         console.log("Plot: " + JSON.parse(body).Plot);
         console.log("Actors: " + JSON.parse(body).Actors);
         if (err) {
-            console.log('err');
+            return console.log('Error occurred: ' + err);
         }
     });
 }
 function doWhatSay() {
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
-            return console.log(err);
+            return console.log('Error occurred: ' + err);
         }
         var output = data.split(",");
         for (var i = 0; i < output.length; i++) {
             console.log(output[i]);
         }
-        spotify.search({ type: "track", query: output[1], limit: 1 }, function (err, data) {
+        spotify.search({ type: 'track', query: output[1], limit: 1 }, function (err, data) {
             if (err) {
-                return console.log('err occurred: ' + err);
+                return console.log('Error occurred: ' + err);
             }
-            console.log(data.name);
-            console.log(JSON.stringify(data, null, 2));
-        });
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                console.log("Artist: " + songData.artists[0].name);
+                console.log("Song Title: " + songData.name);
+                console.log("Preview Track: " + songData.preview_url);
+                console.log("Album: " + songData.album.name);
+            }
+        })
 
     });
 }
